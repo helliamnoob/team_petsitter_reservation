85ad79ebe8f08897d4fbf02cce926c05c90f6038
Yoonzero <playlist808@kakao.com>
2023-07-17 14:32:46 +0900

    유저 마이그레이션파일 및 모델 세팅

---
+++ models/users.js
@@ -0,0 +1,32 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Users extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Users.init(
+        {
+            email: {
+                allowNull: false,
+                unique: true,
+                type: DataTypes.STRING,
+            },
+            password: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'Users',
+        }
+    );
+    return Users;
+};
