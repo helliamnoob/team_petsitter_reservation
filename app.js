45d8e26d4fbe975167bf4db27848a0ae8b4592a3
Yoonzero <playlist808@kakao.com>
2023-07-17 14:52:40 +0900

    회원가입 로그인 확인

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    console.log('연결확인');
-    return res.status(200).json({ message: '안녕' });
-});
+// 서버연결 확인 완료
+// app.get('/', (req, res) => {
+//     console.log('연결확인');
+//     return res.status(200).json({ message: '안녕' });
+// });
 
-// app.use('/', [usersRouter]);
+app.use('/', [usersRouter]);
 
 app.listen(PORT, HOST, () => {
     console.log('Server is listening...', PORT);
--- routes/router.user.js
+++ routes/router.user.js
@@ -5,58 +5,56 @@ const jwt = require('jsonwebtoken');
 
 // 회원가입 API
 router.post('/signup', async (req, res) => {
-    console.log(Users);
-    const { nickname, email, password, confirm } = req.body;
+    const { email, nickname, password, confirm } = req.body;
     try {
-        const confirmedNickname = /^[a-zA-Z0-9]{3,}$/.test(nickname);
-        if (!confirmedNickname) {
-            res.status(412).json({
+        // 이메일 양식 검증
+        const emailCheck = new RegExp(/^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/);
+        if (!emailCheck.test(email))
+            return res.status(412).json({ errorMessage: '이메일의 형식이 올바르지 않습니다.' });
+
+        // 이메일 중복 확인
+        const Email = await Users.findOne({ where: { email } });
+        if (Email) return res.status(412).json({ errorMessage: '이미 존재하는 이메일 입니다.' });
+
+        // 닉네임 형식 확인
+        const nicknameCheck = /^[a-zA-Z0-9]{3,}$/.test(nickname);
+        if (!nicknameCheck) {
+            return res.status(412).json({
                 errorMessage: '닉네임의 형식이 올바르지 않습니다.',
             });
-            return;
         }
 
         // 닉네임 중복 확인
-        const existNickname = await Users.findOne({ where: { nickname } });
-        if (existNickname) {
+        const Nickname = await Users.findOne({ where: { nickname } });
+        if (Nickname) {
             return res.status(412).json({
                 errorMessage: '이미 존재하는 닉네임 입니다.',
             });
         }
 
-        //패스워드 길이 확인 : 4자이상
-        if (password.length < 4) {
-            res.status(412).json({
-                errorMessage: '패스워드의 형식이 올바르지 않습니다.',
-            });
-            return;
+        // 패스워드 형식 확인
+        const checkPassword = new RegExp(/^[a-zA-Z0-9!@#$%^&*()]+$/);
+        if (!checkPassword.test(password) || password.length < 4)
+            return res.status(412).send({ errorMessage: '패스워드의 형식이 올바르지 않습니다.' });
+
+        // 닉네임 패스워드 일치여부 확인
+        if (nickname === password) {
+            return res.status(412).json({ errorMessage: '닉네임과 패스워드가 같습니다.' });
         }
 
-        //패스워드의 값이 닉네임과 동일하지는 않는지 확인
-        // if (password.includes(...nickname)) // 한 글자라도 포함되어 있다면, 사용하지 못하게 하는 코드.
-        if (password.includes(nickname)) {
-            res.status(412).json({
-                errorMessage: '패스워드에 닉네임이 포함되어있습니다.',
-            });
-            return;
-        }
-
-        // 패스워드 확인
+        // 패스워드 일치 확인
         if (password !== confirm) {
-            res.status(412).json({
+            return res.status(412).json({
                 errorMessage: '패스워드가 일치하지 않습니다.',
             });
-            return;
         }
 
         // 회원가입
-        const user = new Users({ nickname, email, password });
+        const user = new Users({ email, nickname, password });
         await user.save();
-
         res.status(201).json({ message: '회원가입을 축하드립니다.' });
     } catch (err) {
         console.log(err);
-        console.log(err.message);
         res.status(400).json({ errorMessage: '요청하신 데이터 형식이 올바르지 않습니다.' });
         return;
     }
@@ -64,24 +62,21 @@ router.post('/signup', async (req, res) => {
 
 // 로그인 API
 router.post('/login', async (req, res) => {
-    const { nickname, password } = req.body; // body에 입력값을 받고,
+    const { email, password } = req.body;
     try {
-        const user = await Users.findOne({ nickname }); // DB에서 입력한 nickname을 가진 user를 찾아 변수에 할당하고,
+        const user = await Users.findOne({ email });
 
-        // DB에 해당하는 nickname이 없거나, 사용자의 password가 일치하지 않는경우.
-        if (!nickname || password !== user.password) {
-            res.status(412).json({ errorMessage: '회원 정보가 일치하지 않습니다.' }); // 해당 오류메시지 출력.
-            return;
+        if (!email || password !== user.password) {
+            return res.status(412).json({ errorMessage: '이메일 또는 패스워드를 확인해 주세요.' });
         }
 
         // jwt 생성
         const token = jwt.sign({ userId: user.userId }, 'customized-secret-key');
 
         // 쿠키 생성
-        res.cookie('Authorization', `Bearer ${token}`); // Authorization이라는 이름의 bearer타입의 토큰 생성.
-        res.status(200).json({ token });
+        res.cookie('Authorization', `Bearer ${token}`).status(200).json({ token });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
         res.status(400).json({ errorMessage: '로그인에 실패하였습니다.' });
     }
 });
