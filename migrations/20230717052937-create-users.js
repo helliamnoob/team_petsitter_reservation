e7b8be3acbb7e69b09a8ad99e05350d828368978
Yoonzero <playlist808@kakao.com>
2023-07-17 20:31:43 +0900

    이메일 인증 오류 진행중 아직 해결 안됨

--- migrations/20230717052937-create-users.js
+++ migrations/20230717052937-create-users.js
@@ -1,39 +1,39 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('Users', {
-            userId: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER,
-            },
-            email: {
-                allowNull: false,
-                unique: true,
-                type: Sequelize.STRING,
-            },
-            nickname: {
-                allowNull: false,
-                unique: true,
-                type: DataTypes.STRING,
-            },
-            password: {
-                allowNull: false,
-                type: Sequelize.STRING,
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-            },
-        });
-    },
-    async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('Users');
-    },
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      userId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      nickname: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
 };
