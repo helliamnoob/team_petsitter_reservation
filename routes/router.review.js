6f416f5f7e97ed28e649f13736063b23b222045f
Juseung Kim <95juseung@gmail.com>
2023-07-18 18:42:10 +0900

    star -> rating 변경

--- routes/router.review.js
+++ routes/router.review.js
@@ -21,10 +21,10 @@ router.post('/petsitters/:petsitter_id/reviews', authMiddleware, async (req, res
   try {
     const { user_id } = res.locals.user;
     const { petsitter_id } = req.params;
-    const { content, star } = req.body;
+    const { content, rating } = req.body;
 
     if (!content) return res.status(412).json({ errorMessage: '내용을 입력해주세요.' });
-    if (!star) return res.status(412).json({ errorMessage: '평점을 입력해주세요.' });
+    if (!rating) return res.status(412).json({ errorMessage: '평점을 입력해주세요.' });
     // 해당 펫시터 사용 경험이 없을 경우 리뷰 작성 권한이 없어야 하는데 어떤식으로 구현해야 할까요?
 
     await Reviews.create({
@@ -45,14 +45,14 @@ router.put('/reviews/:review_id', authMiddleware, async (req, res) => {
   try {
     const { user_id } = res.locals.user;
     const { review_id } = req.params;
-    const { content, star } = req.body;
+    const { content, rating } = req.body;
 
     const review = await Reviews.findOne({ where: { review_id } });
 
     if (user_id !== review.User_id)
       return res.status(403).json({ errorMessage: '댓글의 수정 권한이 없습니다.' });
     if (!content) return res.status(412).json({ errorMessage: '내용을 입력해주세요.' });
-    if (!star) return res.status(412).json({ errorMessage: '평점을 입력해주세요.' });
+    if (!rating) return res.status(412).json({ errorMessage: '평점을 입력해주세요.' });
 
     await Reviews.update({ content, star }, { where: { review_id } });
 
