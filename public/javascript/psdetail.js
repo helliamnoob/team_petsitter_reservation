31651c96a0fbb79c9b88e9da5212f607abf7242f
helloiamnoob <86592051+helliamnoob@users.noreply.github.com>
2023-07-24 09:04:02 +0900

    캘린더 과거 일정 막기, 중복 예약 막기 완료
    캘린더 과거 일정 막기, 중복 예약 막기 완료

--- public/javascript/psdetail.js
+++ public/javascript/psdetail.js
@@ -2,21 +2,20 @@ let url = location.href;
 let idx = url.indexOf('=');
 let id;
 if (idx >= 0) {
-   idx = idx + 2;
-   id = url.substring(idx, url.length);
+  idx = idx + 2;
+  id = url.substring(idx, url.length);
 }
 let status = url.indexOf('petsitters');
 let post = false;
-if( status >= 0){
-    post = true;
+if (status >= 0) {
+  post = true;
 }
 console.log(post);
 
-if(post === true)
-{
-document.addEventListener('DOMContentLoaded', async function () {
+if (post === true) {
+  document.addEventListener('DOMContentLoaded', async function () {
     let calendarEl = document.getElementById('calendar');
-  
+
     let calendar = new FullCalendar.Calendar(calendarEl, {
       selectable: true,
       headerToolbar: {
@@ -26,8 +25,48 @@ document.addEventListener('DOMContentLoaded', async function () {
       },
       dateClick: async function (info) {
         alert('selected ' + info.date);
-        const response = await fetch(`/petsitters/${id}/reservations`, {
-          method: 'POST',
+
+        const check = info.date;
+        const today = new Date();
+        if (check < today) {
+          alert('과거는 예약할 수 없습니다.');
+        } else {
+          const response = await fetch(`/petsitters/${id}/reservations`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ start_date: info.date, end_date: info.date }),
+          });
+          await response.json().then((result) => {
+            const errorMessage = result.errorMessage;
+            if (errorMessage) {
+              alert(result.errorMessage);
+            } else {
+              alert(result.message);
+            }
+            window.location.href = '/reservation';
+          });
+        }
+      },
+    });
+    calendar.render();
+  });
+} else if (post === false) {
+  document.addEventListener('DOMContentLoaded', async function () {
+    let calendarEl = document.getElementById('calendar');
+
+    let calendar = new FullCalendar.Calendar(calendarEl, {
+      selectable: true,
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+      },
+      dateClick: async function (info) {
+        alert('selected ' + info.date);
+        const response = await fetch(`/reservations/${id}`, {
+          method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
           },
@@ -40,45 +79,10 @@ document.addEventListener('DOMContentLoaded', async function () {
           } else {
             alert(result.message);
           }
-          window.location.href='/reservation'
+          window.location.href = '/reservation';
         });
       },
     });
     calendar.render();
   });
-}
-else if(post === false)
-{
-    document.addEventListener('DOMContentLoaded', async function () {
-        let calendarEl = document.getElementById('calendar');
-      
-        let calendar = new FullCalendar.Calendar(calendarEl, {
-          selectable: true,
-          headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay',
-          },
-          dateClick: async function (info) {
-            alert('selected ' + info.date);
-            const response = await fetch(`/reservations/${id}`, {
-              method: 'PUT',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ start_date: info.date, end_date: info.date }),
-            });
-            await response.json().then((result) => {
-              const errorMessage = result.errorMessage;
-              if (errorMessage) {
-                alert(result.errorMessage);
-              } else {
-                alert(result.message);
-              }
-              window.location.href='/reservation'
-            });
-          },
-        });
-        calendar.render();
-      });
 }
